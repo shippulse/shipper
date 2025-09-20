fc9f7a2e8464db31e1b723cf2c96221ada9070df
Karim Mohamed <komicho1996@gmail.com>
2025-09-20 20:49:21 +0300

    Enhance ShippingProviderInterface with additional shipment management methods

--- src/Contracts/ShippingProviderInterface.php
+++ src/Contracts/ShippingProviderInterface.php
@@ -7,4 +7,10 @@ use Obelaw\Shippulse\Shipper\Contracts\ShipmentDataInterface;
 interface ShippingProviderInterface
 {
     public function createShipment(ShipmentDataInterface $data);
+
+    public function trackShipment($shipmentId);
+
+    public function labelShipment($shipmentId);
+
+    public function cancelShipment($shipmentId);
 }
